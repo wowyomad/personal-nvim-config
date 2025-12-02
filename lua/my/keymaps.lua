b92f0df0df0d1ad50e7496f5cde8275e5ea79feb
wowyomad <vadimsun8837@gmail.com>
2025-12-02 04:04:48 +0300

    Initial Nevim config

---
+++ lua/my/keymaps.lua
@@ -0,0 +1,33 @@
+
+vim.g.mapleader = ' '
+
+-- makes it possible to leave 'TERMINAL' mode with just ESC.
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-N>', { noremap = true, silent = true })
+
+-- '<Leader>l toggles line number mode (number, relativenumber, both, off) 
+vim.keymap.set('n', '<Leader>l', function()
+    local n = vim.opt.number:get()
+    local rn = vim.opt.relativenumber:get()
+    
+    local function reset_statuscolumn()
+        vim.opt.statuscolumn = ''
+    end
+
+    if n and not rn then
+        vim.opt.number = false
+        vim.opt.relativenumber = true
+        reset_statuscolumn()
+    elseif not n and rn then
+        vim.opt.number = true
+        vim.opt.relativenumber = true
+        vim.opt.statuscolumn = '%{v:lnum} %{v:relnum}'
+    elseif n and rn then
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+        reset_statuscolumn()
+    else
+        vim.opt.number = true
+        vim.opt.relativenumber = false
+        reset_statuscolumn()
+    end
+end)
---
+++ this-file-wont-load.lua
@@ -0,0 +1,2 @@
+print("This won't be displayed when nvim starts")
+print("This as well")
